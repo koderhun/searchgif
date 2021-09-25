937eaff3f7b60419001e59813fb2889fa5729a48
Ramil <koderhanov@gmail.com>
2021-09-25 04:20:54 +0300

    copy

--- src/components/HeaderPage/index.jsx
+++ src/components/HeaderPage/index.jsx
@@ -1,6 +1,6 @@
 import React from "react"
 import { Link } from "react-router-dom"
-import { Layout, Menu } from "antd"
+import { Layout, Menu, Space } from "antd"
 import { HeartTwoTone } from "@ant-design/icons"
 import styles from "./styles.module.scss"
 
@@ -9,17 +9,24 @@ const { Header } = Layout
 export const HeaderPage = () => {
   return (
     <Header className={styles.header}>
-      <HeartTwoTone
-        className={styles.logo}
-        twoToneColor="#eb2f96"
-        style={{ fontSize: "20px" }}
-      />
+      <div className={styles.left}>
+        <HeartTwoTone
+          className={styles.logo}
+          twoToneColor="#eb2f96"
+          style={{ fontSize: "20px" }}
+        />
 
-      <Menu theme="dark" mode="horizontal">
-        <Menu.Item>
-          <Link to="/searchgif">Home</Link>
-        </Menu.Item>
-      </Menu>
+        <Menu theme="dark" mode="horizontal">
+          <Menu.Item>
+            <Link to="/searchgif">Home</Link>
+          </Menu.Item>
+        </Menu>
+      </div>
+      <div className={styles.right}>
+        <span className={styles.copyright}>
+          Power by <a href="https://giphy.com/">Giphy</a>
+        </span>
+      </div>
     </Header>
   )
 }
